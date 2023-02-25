27d45f3450bd92fff2dc024afeaa947616896483
shuja <shuja.gmail.com>
2023-02-25 21:57:46 +0530

    variable types

---
+++ 02-var/var.tf
@@ -0,0 +1,16 @@
+variable "sample" {
+  default = "Hello from Default "
+}
+variable "list" {
+  default = [ "shuja",100,boolean]
+}
+variable "map" {
+  default = {
+    name = "shujath",
+    course = "Cloud",
+    boolean = true
+  }
+}
+output "list" {
+  value = "first value from list ${var.list[0]}, 3rd value from map is ${var.map[boolean]} "
+}
