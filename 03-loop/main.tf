2a009e7e2a0da3c7e69513e82639dc511601c19f
shuja <shuja.gmail.com>
2023-02-26 23:03:17 +0530

    var d2

--- 03-loop/main.tf
+++ 03-loop/main.tf
@@ -4,3 +4,9 @@ variable "d1" {
 resource "null_resource" "nothing" {
   count = var.d1
 }
+variable "d2" {
+  default = [banana, apple]
+}
+resource "null_resource" "d2" {
+  count = length(var.d2)
+}
