fc04d6095ef913f03972af83656673e705e9d83b
shuja <shuja.gmail.com>
2023-02-26 23:05:34 +0530

    quotations added

--- 03-loop/main.tf
+++ 03-loop/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "nothing" {
   count = var.d1
 }
 variable "d2" {
-  default = [banana, apple]
+  default = ["banana", "apple"]
 }
 resource "null_resource" "d2" {
   count = length(var.d2)
