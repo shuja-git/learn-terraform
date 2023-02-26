23bce6f1169227a8a2aca27eb4ccc31b6d143312
shuja <shuja.gmail.com>
2023-02-26 16:01:30 +0530

    count example

--- 02-var/var.tf
+++ 02-var/var.tf
@@ -1,24 +1,42 @@
-variable "sample" {
-  default = "Hello from Default "
-}
-variable "list" {
+#variable "sample" {
+#  default = "Hello from Default "
+#}
+#variable "list" {
+#  default = [
+#    "shuja",
+#    100,
+#    true]
+#}
+#
+#
+#variable "map" {
+#  default = {
+#    name = "shujath",
+#    course = "Cloud",
+#    boolean = true
+#  }
+#}
+#output "list" {
+#  value = "first value from list ${var.list[0]}"
+#}
+#output "vars" {
+#  value = "last value from map is ${var.map["boolean"]}"
+#}
+#----------------
+#Count example
+#----------------
+variable "d1" {
   default = [
-    "shuja",
-    100,
-    true]
+    {
+      course_name = "aws"
+      trainer_name = "Raghu"
+    },
+    {
+      course_name = "DevOps"
+      trainer_name = "Imran"
+    },
+  ]
 }
-
-
-variable "map" {
-  default = {
-    name = "shujath",
-    course = "Cloud",
-    boolean = true
-  }
-}
-output "list" {
-  value = "first value from list ${var.list[0]}"
-}
-output "vars" {
-  value = "last value from map is ${var.map["boolean"]}"
+output "count" {
+  value = var.d1.*.course_name
 }
