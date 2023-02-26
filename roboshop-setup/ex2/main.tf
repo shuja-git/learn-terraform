c0ae4c6bb90d82c0e3810e42a64dadd8afe7e1e6
shuja <shuja.gmail.com>
2023-02-26 22:42:18 +0530

    creating 5 instances using count

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "frontend" {
+  count = length(var.instances)
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0f814c32290173c7c"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+variable "instances" {
+  default = ["cart","catalogue","user","shipping","payment"]
+}
+output "public_ip" {
+  value = aws_instance.frontend.*.public_ip
+}
