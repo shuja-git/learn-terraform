4a5deee22ee5bed082ba6f7cfb4f35b4d59e4d0f
shuja <shuja.gmail.com>
2023-03-01 11:35:15 +0530

    testing ec2 module output

--- roboshop-setup/ex5/ec2/main.tf
+++ roboshop-setup/ex5/ec2/main.tf
@@ -1,9 +1,18 @@
-#data "aws_ami" "ami" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-#
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 #resource "aws_instance" "instances" {
 #  ami           = data.aws_ami.ami.image_id
 #  instance_type =
--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -1,6 +1,13 @@
 module "security_group" {
   source = "./sg"
 }
+module "ec2_instance" {
+  source = "./ec2"
+}
+
 output "sg_id" {
   value = module.security_group
+}
+output "ec2_output" {
+  value = module.ec2_instance
 }
