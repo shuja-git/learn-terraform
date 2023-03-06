9efd623b205516fac31e71bbe0589ad444fbabe0
shuja <shuja.gmail.com>
2023-03-06 15:07:08 +0530

    accessing list and map values

--- 02-var/var.tf
+++ 02-var/var.tf
@@ -1,50 +1,50 @@
 #variable "sample" {
 #  default = "Hello from Default "
 #}
-#variable "list" {
-#  default = [
-#    "shuja",
-#    100,
-#    true]
-#}
+variable "list" {
+  default = [
+    "shuja",
+    100,
+    true]
+}
 #
 #
-#variable "map" {
-#  default = {
-#    name = "shujath",
-#    course = "Cloud",
-#    boolean = true
-#  }
-#}
-#output "list" {
-#  value = "first value from list ${var.list[0]}"
-#}
+variable "map" {
+  default = {
+    name = "shujath",
+    course = "Cloud",
+    boolean = true
+  }
+}
+output "list" {
+  value = "first value from list ${var.list[0]}, boolean value from map is ${var.map["boolean"]}"
+}
 #output "vars" {
 #  value = "last value from map is ${var.map["boolean"]}"
 #}
 #----------------
 #Count example
 #----------------
-variable "d1" {
-  default = [
-    {
-      course_name = "aws"
-      trainer_name = "Raghu"
-    },
-    {
-      course_name = "DevOps"
-      trainer_name = "Imran"
-    },
-  ]
-}
+#variable "d1" {
+#  default = [
+#    {
+#      course_name = "aws"
+#      trainer_name = "Raghu"
+#    },
+#    {
+#      course_name = "DevOps"
+#      trainer_name = "Imran"
+#    },
+#  ]
+#}
 #output "count" {
 #  value = var.d1[count.index]
 #}
-variable "sample1" {}
-output "sample1" {
-  value = var.sample1
-}
-variable "cli" {}
-output "cli" {
-  value = var.cli
-}
+#variable "sample1" {}
+#output "sample1" {
+#  value = var.sample1
+#}
+#variable "cli" {}
+#output "cli" {
+#  value = var.cli
+#}
