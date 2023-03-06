631a40d822ffd20f8857649359e0a334dce95a2d
shuja <shuja.gmail.com>
2023-03-06 21:35:28 +0530

    count example

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -6,17 +6,14 @@ data "aws_ami" "ami" {
 
 
 resource "aws_instance" "frontend" {
-  count = length(var.instances)
+  count = 5
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0f814c32290173c7c"]
-  tags = {
-    Name = var.instances[count.index]
   }
-}
-variable "instances" {
-  default = ["cart","catalogue","user","shipping","payment"]
-}
-output "public_ip" {
-  value = aws_instance.frontend.*.public_ip
-}
+#variable "instances" {
+#  default = ["cart","catalogue","user","shipping","payment"]
+#}
+#output "public_ip" {
+#  value = aws_instance.frontend.*.public_ip
+#}
