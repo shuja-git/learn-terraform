27c03395978d6321e8bd49e9032162baaab48650
shuja <shuja.gmail.com>
2023-03-06 21:53:12 +0530

    printing multiple values by count

--- 02-var/var.tf
+++ 02-var/var.tf
@@ -1,40 +1,40 @@
 variable "sample1" {}
 #  default = "Hello from Default "
 #}
-variable "list" {
-  default = [
-    "shuja",
-    100,
-    true]
-}
+#variable "list" {
+#  default = [
+#    "shuja",
+#    100,
+#    true]
+#}
+##
+##
+#variable "map" {}
 #
-#
-variable "map" {}
-
-output "list" {
-  value = "first value from list ${var.list[0]}, boolean value from map is ${var.map["boolean"]}"
-}
+#output "list" {
+#  value = "first value from list ${var.list[0]}, boolean value from map is ${var.map["boolean"]}"
+#}
 #output "vars" {
 #  value = "last value from map is ${var.map["boolean"]}"
 #}
 #----------------
 #Count example
 #----------------
-#variable "d1" {
-#  default = [
-#    {
-#      course_name = "aws"
-#      trainer_name = "Raghu"
-#    },
-#    {
-#      course_name = "DevOps"
-#      trainer_name = "Imran"
-#    },
-#  ]
-#}
-#output "count" {
-#  value = var.d1[count.index]
-#}
+variable "d1" {
+  default = [
+    {
+      course_name = "aws"
+      trainer_name = "Raghu"
+    },
+    {
+      course_name = "DevOps"
+      trainer_name = "Imran"
+    },
+  ]
+}
+output "count" {
+  value = var.d1.*.course_name
+}
 #variable "sample1" {}
 #output "sample1" {
 #  value = var.sample1
