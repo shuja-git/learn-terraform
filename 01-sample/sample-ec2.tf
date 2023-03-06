48dffaad6edb13783888f4c483c3c36452d9756c
shuja <shuja.gmail.com>
2023-03-06 11:29:28 +0530

    tags added

--- 01-sample/sample-ec2.tf
+++ 01-sample/sample-ec2.tf
@@ -2,4 +2,7 @@ resource "aws_instance" "ec2-sample" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0f814c32290173c7c"]
+  tags = {
+    name = "test"
+  }
 }
