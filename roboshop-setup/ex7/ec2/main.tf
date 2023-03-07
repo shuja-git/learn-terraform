a2fe4ca6be6237b098cb2c58b2fcb4a57c16f051
shuja <shuja.gmail.com>
2023-03-07 12:41:01 +0530

    refering sg module in ec2 module

---
+++ roboshop-setup/ex7/ec2/main.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [var.sg_id]
+  tags = {
+    name = var.component
+  }
+}
+variable "sg_id" {}
+
+
