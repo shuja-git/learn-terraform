2a1c537422de7b6f7c9adf7721bdfda8f2c9d767
shuja <shuja.gmail.com>
2023-03-07 10:09:01 +0530

    printing key and value

--- 02-output/out.tf
+++ 02-output/out.tf
@@ -29,6 +29,6 @@ variable "instances" {
 }
 
 output "ec2" {
-  value = [for k, v in aws_instance.instances : v.public_ip ]
+  value = [ for k, v in aws_instance.instances : "${k} - ${v.public_ip}" ]
 
 }
