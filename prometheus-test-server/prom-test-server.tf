e968a068c92d8f8cd892de4df51aaffdc61be770
shuja <shuja.gmail.com>
2023-03-24 19:53:42 +0530

    spot req

--- prometheus-test-server/prom-test-server.tf
+++ prometheus-test-server/prom-test-server.tf
@@ -1,4 +1,4 @@
-resource "aws_spot_instance_request" "node1" {
+resource "aws_spot_instance_request" "Node1" {
   ami           = "ami-0b9af232e8488cd98"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0f814c32290173c7c"]
@@ -9,7 +9,7 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
-resource "aws_spot_instance_request" "node2" {
+resource "aws_spot_instance_request" "Node2" {
   ami           = "ami-0b9af232e8488cd98"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0f814c32290173c7c"]
